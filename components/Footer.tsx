672995a3f32f1e97415d7b91a5ef3f79bf38ff42
Farid0558475484 <faridmh@code.edu.az>
2023-04-06 18:50:34 +0400

    feat:add Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,56 @@
 import { FC } from "react";
+import { Button } from "react-bootstrap";
+import styles from "../styles/Footer.module.scss";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Footer: FC = () => (
-  <footer>
-    <p>Created by Farid Hacizade</p>
-  </footer>
+  <Container>
+    <Row className={styles.footer}>
+      <Col md="6" className={styles.info}>
+        <div className={styles.phone}>
+          <div className={styles.phone1}>
+            <a href="tel:+994508591717">
+              <Button variant="success">Zeng et:</Button>
+            </a>
+            <span> 0508591717</span>
+          </div>
+          <div className={styles.phone2}>
+            <a href="tel:+994708591717">
+              <Button variant="success">Zeng et:</Button>
+            </a>
+            <span>0708591717</span>
+          </div>
+        </div>
+        <div className={styles.email}>
+          <a href="mailto:info@akbagro.az">
+            <Button variant="success">Email</Button>
+          </a>
+          <span> info@akbagro.az</span>
+        </div>
+        <div className={styles.location}>
+          <a href="">
+            <Button variant="success">Unvan</Button>
+          </a>
+          <div>
+            <span>Lökbatan qəsəbəsi Xocahəsən,</span>
+            <br />
+            <span>yolu 57 korpus 8, mağaza 13,14</span>
+          </div>
+        </div>
+      </Col>
+
+      <Col md="6" className={styles.map}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22860.76515080142!2d49.72151872821744!3d40.3303700899744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307f811a40fc4b%3A0xf75cb330fdbf2716!2zWG9jYWjJmXPJmW4!5e0!3m2!1sru!2saz!4v1680789467407!5m2!1sru!2saz"
+          width="600"
+          height="400"
+        ></iframe>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Footer;
