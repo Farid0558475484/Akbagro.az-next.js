0980208102949633e0244246eaa60393303dfa89
Farid0558475484 <faridmh@code.edu.az>
2023-04-06 14:54:46 +0400

    feat:backgraoundcolor

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -13,47 +13,48 @@ function Menu() {
 
   return (
     <>
-      <div className={s.MobileMenu}>
-        <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={50} height={50} />
-        </Link>
-        <a href="tel:+99477384242">
-          <Button variant="success">Zeng et</Button>
-        </a>
-        <Button variant="primary" onClick={handleShow}>
-          Menu
-        </Button>
-      </div>
+      <div className={s.bodyMenu}>
+        <div className={s.MobileMenu}>
+          <Link href="/">
+            <Image src="/logo.png" alt="Logo" width={50} height={50} />
+          </Link>
+          <a href="tel:+99477384242">
+            <Button variant="success">Zeng et</Button>
+          </a>
+          <Button variant="primary" onClick={handleShow}>
+            Menu
+          </Button>
+        </div>
 
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title className={s.title}>
-            {" "}
-            <li onClick={handleClose}>
-              <Link href="/"> AkbAgro.az</Link>
-            </li>
-       
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <div>
-            <ul className={s.navbar__links}>
+        <Offcanvas show={show} onHide={handleClose} className={s.bodyMenu}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title className={s.title}>
+              {" "}
               <li onClick={handleClose}>
-                <Link href="/about-us">About</Link>
+                <Link href="/"> AkbAgro.az</Link>
               </li>
-              <li onClick={handleClose}>
-                <Link href="/products">Products</Link>
-              </li>
-              <li onClick={handleClose}>
-                <Link href="/services">Services</Link>
-              </li>
-              <li onClick={handleClose}>
-                <Link href="/contact">Contact</Link>
-              </li>
-            </ul>
-          </div>
-        </Offcanvas.Body>
-      </Offcanvas>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <div>
+              <ul className={s.navbar__links}>
+                <li onClick={handleClose}>
+                  <Link href="/about-us">About</Link>
+                </li>
+                <li onClick={handleClose}>
+                  <Link href="/products">Products</Link>
+                </li>
+                <li onClick={handleClose}>
+                  <Link href="/services">Services</Link>
+                </li>
+                <li onClick={handleClose}>
+                  <Link href="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </div>
     </>
   );
 }
