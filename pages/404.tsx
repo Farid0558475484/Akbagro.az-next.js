400643733734387762d94212157e336f1ee2162a
Farid0558475484 <faridmh@code.edu.az>
2023-04-06 15:06:24 +0400

    feat:add 404 pages

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import s from "../styles/404.module.scss";
+
+const Error = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, [router]);
+
+  return (
+    <div className={s.wrapper}>
+      <h1>404 - Bele bir seyfe yoxdu</h1>
+    </div>
+  );
+};
+
+export default Error;
