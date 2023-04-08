3baa6f7c2ee8346b7403be5ca7301e4c6e585e7a
Farid0558475484 <faridmh@code.edu.az>
2023-04-08 16:00:27 +0400

    feat:product card

---
+++ pages/api/cards.ts
@@ -0,0 +1,147 @@
+ export  const cards = [
+    {
+      imageSrc: "/p1.webp",
+      title: "Kurtağzı çıxış nipeli",
+    },
+    {
+      imageSrc: "/p1.webp",
+      title: "PE tee inegal",
+    },
+    {
+      imageSrc: "/p1.webp",
+      title: "PE tee inegal",
+    },
+    {
+      imageSrc: "/p1.webp",
+      title: "PE tee inegal",
+    },
+    {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "Kurtağzı çıxış nipeli",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      {
+        imageSrc: "/p1.webp",
+        title: "PE tee inegal",
+      },
+      
+  ];
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -5,75 +5,32 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Image from "next/image";
 import s from "./../../styles/Products.module.scss";
+import { cards } from "./../api/cards";
 
 function Products() {
   return (
     <Container>
       <Row className="mt-5 mb-5">
-        <Col xl="3" xs="6">
-          <Card className={s.card}>
-            <Image
-              src="/p1.webp"
-              alt="Logo"
-              width={100}
-              height={100}
-              layout="responsive"
-              objectFit="cover"
-              priority
-            />
-            <Card.Body className={s.card}>
-              <Card.Title className={s.card}>Kurtağzı çıxış nipeli</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xl="3" xs="6">
-          <Card className={s.card}>
-            <Image
-              src="/p1.webp"
-              alt="Logo"
-              width={100}
-              height={100}
-              layout="responsive"
-              objectFit="cover"
-              priority
-            />
-            <Card.Body className={s.card}>
-              <Card.Title className={s.card}>PE tee inegal</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xl="3" xs="6">
-          <Card>
-            <Image
-              src="/p1.webp"
-              alt="Logo"
-              width={100}
-              height={100}
-              layout="responsive"
-              objectFit="cover"
-              priority
-            />
-            <Card.Body>
-              <Card.Title>PE flans adaptor</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xl="3" xs="6">
-          <Card>
-            <Image
-              src="/p1.webp"
-              alt="Logo"
-              width={100}
-              height={100}
-              layout="responsive"
-              objectFit="cover"
-              priority
-            />
-            <Card.Body>
-              <Card.Title>Polietilen borular</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
+        {cards.map((card, index) => (
+          <Col xl="3" xs="6" key={index}  className={s.cardCol}>
+            <Card className={s.card}>
+              <Image
+                src={card.imageSrc}
+                alt="Logo"
+                width={100}
+                height={100}
+                layout="responsive"
+                objectFit="cover"
+                priority
+                blurDataURL='data:{card.imageSrc};base64,[base64-encoded-placeholder-image]'
+              
+              />
+              <Card.Body className={s.cardBody}>
+                <Card.Title className={s.cardBody}>{card.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
