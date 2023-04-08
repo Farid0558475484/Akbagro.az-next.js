7494a8fb938485415a8539264d6cc85116460c8e
Farid0558475484 <faridmh@code.edu.az>
2023-04-08 11:29:38 +0400

    feat:Head

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
 import Navbar from "./Navbar";
 import { FC } from "react";
 import Menu from "./Menu";
+import Head from "next/head";
 
 const Header: FC = () => (
   <header>
+    <Head>
+      <title>AKB Agro</title>
+      <meta name="description" content="AKB Agro suvarma sistemləri" />
+      <link rel="icon" href="/logo.png" />
+    </Head>
     <Navbar />
     <Menu />
   </header>
